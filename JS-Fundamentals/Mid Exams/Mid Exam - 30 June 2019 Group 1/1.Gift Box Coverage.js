2899dfc025ee1d931fd6706b18359a959a4e84b5
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2019-07-02 18:40:33 +0300

    refactoring problems 1 and 3

--- JS-Fundamentals/Mid Exams/Mid Exam - 30 June 2019 Group 1/1.Gift Box Coverage.js
+++ JS-Fundamentals/Mid Exams/Mid Exam - 30 June 2019 Group 1/1.Gift Box Coverage.js
@@ -4,31 +4,24 @@ function solve(input) {
     let areaSingleSheet = Number(input.shift());
 
     let areaNeeded = sizeOfSide * sizeOfSide * 6;
-    let areaHave = areaSingleSheet * sheetsOfPaper;
-    let a = 0
-    let b = 0
+    let everyThirdSheetCount = 0
+    let sheetsCount = 0
     for (let sheet = 1; sheet <= sheetsOfPaper; sheet++) {
-        if (areaHave <= 0) {
-            break;
-        }
         if (sheet % 3 === 0) {
-            a++
-        }else {
-            b++
+            everyThirdSheetCount++
+        } else {
+            sheetsCount++
         }
-
     }
-    a = a * areaSingleSheet * 0.25;
-    b = b * areaSingleSheet
- let totalCovered = a + b;
-
-let result = totalCovered / areaNeeded * 100;
+    everyThirdSheetCount *= areaSingleSheet * 0.25;
+    sheetsCount *= areaSingleSheet
+    let totalCovered = everyThirdSheetCount + sheetsCount;
+    let result = totalCovered / areaNeeded * 100;
     console.log(`You can cover ${result.toFixed(2)}% of the box.`)
-
 }
 
 solve(['5', '30', '4']);
-solve(['2.5',
-    '32',
-    '4.25'
-    ])
+// solve(['2.5',
+//     '32',
+//     '4.25'
+//     ])
--- JS-Fundamentals/Mid Exams/Mid Exam - 30 June 2019 Group 1/3.Froggy Squad.js
+++ JS-Fundamentals/Mid Exams/Mid Exam - 30 June 2019 Group 1/3.Froggy Squad.js
@@ -1,26 +1,25 @@
 function solve(input) {
     let list = input.shift().split(' ');
     for (const line of input) {
-        let [command, name, index] = line.split(' ');
+        let [command, nameOrIndex, index] = line.split(' ');
         index = Number(index);
-        if (command === 'Join' && !list.includes(name)) {
-            list.push(name);
+        if (command === 'Join' && !list.includes(nameOrIndex)) {
+            list.push(nameOrIndex);
         } else if (command == 'Jump' && index >= 0 && index < list.length) {
-            list.splice(index, 0, name);
-        } else if (command === 'Dive' && Number(name) >= 0 && Number(name) < list.length) {
-            name = Number(name);
-            list.splice(name, 1);
+            list.splice(index, 0, nameOrIndex);
+        } else if (command === 'Dive' && Number(nameOrIndex) >= 0 && Number(nameOrIndex) < list.length) {
+            list.splice(nameOrIndex, 1);
         } else if (command === 'First') {
-            name = Number(name);
-            let result = list.slice(0, name);
+            let count = Number(nameOrIndex);
+            let result = list.slice(0, count);
             console.log(result.join(' '))
         } else if (command === 'Last') {
+            let count = Number(nameOrIndex)
             let result = list.reverse()
-                .slice(0, name);
+                             .slice(0, count);
             result.reverse();
             list.reverse();
             console.log(result.join(' '));
-           
         } else if (line === 'Print Reversed') {
             list.reverse();
             console.log(`Frogs: ${list.join(' ')}`);
@@ -30,17 +29,17 @@ function solve(input) {
 }
 
 
-// solve([
-//     'Blake Muggy Kishko',
-//     'Join Kvachko',
-//     'Dive 0',
-//     'First 10',
-//     'Print Reversed']);
+solve([
+    'Blake Muggy Kishko',
+    'Join Kvachko',
+    'Dive 0',
+    'First 10',
+    'Print Reversed']);
 
-    solve([ 'A B C D E F',
-    'Join G',
-    'Jump Q 3',
-    'Last 3',
-    'Dive 2',
-    'Print Normal' ]
-  )
+//     solve([ 'A B C D E F',
+//     'Join G',
+//     'Jump Q 3',
+//     'Last 3',
+//     'Dive 2',
+//     'Print Normal' ]
+//   )
