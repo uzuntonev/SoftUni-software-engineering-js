feeac74d8a8052487a933d4322baae4e67c9a98e
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2019-07-15 00:14:50 +0300

    Update solution.js

--- JS-core-may-2019/02. Chat-Room/solution.js
+++ JS-core-may-2019/02. Chat-Room/solution.js
@@ -3,11 +3,11 @@ function solve() {
    let button = document.getElementById('send');
    button.addEventListener('click', () => {
       let inputField = document.getElementById('chat_input');
-      let textField = inputField.value;
+      let text = inputField.value;
       inputField.value = '';
 
       let newMessage = document.createElement('div');
-      newMessage.textContent = textField;
+      newMessage.textContent = text;
 
       let att = document.createAttribute("class"); 
       att.value = "message my-message";
@@ -17,5 +17,4 @@ function solve() {
       parent.appendChild(newMessage)
    });
 }
-
 
