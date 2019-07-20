05a7f66a8b6d72689fdc3aac207cde8f41b1d4c4
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2019-07-20 12:34:52 +0300

    Update 1.Furniture.js

--- JS-fundamentals-may-2019/Homeworks/Regular-expressions/1.Furniture.js
+++ JS-fundamentals-may-2019/Homeworks/Regular-expressions/1.Furniture.js
@@ -7,10 +7,10 @@ function solve(input) {
             break;
         }
 
-        const pattern = />>(?<name>.*)<<(?<price>[0-9]+|[0-9]+\.[0-9]+)!(?<quantity>[0-9]{1,})/gi
+        const pattern = />>(?<name>[A-Za-z]+)<<(?<price>[0-9\.]+)!(?<quantity>[0-9]{1,})/gi
         if (line.match(pattern)) {
             // const [furniture, price, quntity] = line.split(/[>><<!]/g).filter(e => e.length);
-            let list = pattern.exec(line);
+            const list = pattern.exec(line);
 
             sum += list.groups.price * list.groups.quantity
             console.log(`${list.groups.name}`)
@@ -18,31 +18,7 @@ function solve(input) {
         }
     }
     console.log(`Total money spend: ${sum.toFixed(2)}`)
-
 }
 
 solve(['>>Sofa<<312.23!3', '>>TV<<300!5', '>Invalid<<!5', 'Purchase']
 );
-
-
-
-// furn = (arr) => {
-//     arr.pop()
-//     let total = 0
-
-//     let regex = />>(?<name>.*)<<(?<price>[0-9]+|[0-9]+\.[0-9]+)!(?<quantity>[0-9]{1,})/gi
-//     console.log(`Bought furniture:`)
-//     arr.forEach(element => {
-//         if (element.match(regex)) {
-
-//             let obj = regex.exec(element)
-
-//             let price = obj.groups.price;
-//             let quantity = obj.groups.quantity;
-//             total += quantity * price
-//             console.log(obj.groups.name)
-//         }
-//     });
-//     console.log(`Total money spend: ${total.toFixed(2)}`)
-// }
-// furn(['>>Sofa<<312.23!3', '>>TV<<300!5', '>Invalid<<!5', 'Purchase'])
