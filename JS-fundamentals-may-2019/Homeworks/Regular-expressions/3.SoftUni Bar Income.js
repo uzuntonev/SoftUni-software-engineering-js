041a0dc44811be367ae09903efeee73e3b225cdf
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2019-07-22 09:58:29 +0300

    Update 3.SoftUni Bar Income.js

--- JS-fundamentals-may-2019/Homeworks/Regular-expressions/3.SoftUni Bar Income.js
+++ JS-fundamentals-may-2019/Homeworks/Regular-expressions/3.SoftUni Bar Income.js
@@ -1,33 +1,17 @@
 function solve(input) {
-    const pattern = /\%[A-Z][a-z]+\%([^\|$%\.]*)\<[A-Za-z]+\>([^\|$%\.]*)\|[0-9]+\|([^\|$%\.]*)[0-9\.]+\$/g;
+    const pattern = /%(?<name>[A-Z][a-z]+)%[^\|$%\.]*<(?<item>[\w]+)>[^\|$%\.]*\|(?<quantity>[0-9]+)\|([^\d\|$%\.]*)(?<price>[0-9\.]+)\$/g;
     let total = 0;
+
+
     for (const line of input) {
         if (line === 'end of shift') {
             break;
         }
         if (line.match(pattern)) {
-            const patternName = /%[A-Z][a-z]+%/g;
-            const patternItem = /<[A-Za-z]+>/g;
-            const patternQuantity = /\|[0-9]+\|/g;
-            const patternPrice = /[0-9\.]+\$/g;
-            const name = line
-                .match(patternName)[0]
-                .split('%')
-                .filter(e => e.length)[0];
-            const item = line
-                .match(patternItem)[0]
-                .split(/[<>]/g)
-                .filter(e => e.length)[0];
-            const qunatity = Number(line
-                .match(patternQuantity)[0]
-                .split('|')
-                .filter(e => e.length)[0]);
-            const price = Number(line
-                .match(patternPrice)[0]
-                .split('$')
-                .filter(e => e.length)[0]);
-            total += qunatity * price;
-            console.log(`${name}: ${item} - ${(price * qunatity).toFixed(2)}`)
+            let result = pattern.exec(line);
+            const productsPrice = +result.groups.quantity * +result.groups.price
+            total += productsPrice;
+            console.log(`${result.groups.name}: ${result.groups.item} - ${productsPrice.toFixed(2)}`)
         }
     }
     console.log(`Total income: ${total.toFixed(2)}`)
@@ -43,6 +27,6 @@ solve([
     '%InvalidName%<Croissant>|2|10.3$',
     '%Peter%<Gum>1.3$',
     '%Maria%<Cola>|1|2.4',
-    '%Valid%<Valid>vali42d|10|val424id20$',
+    '%Valid%<Valid>valid|10|valid20$',
     'end of shift']
 );
