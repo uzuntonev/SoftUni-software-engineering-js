3520ed29db9bdf1deb28b50efdee994ab2e42309
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2019-10-02 23:02:50 +0300

    DOM manipulations upload

--- JS-advanced-september-2019/06. DOM Manipulations/Lab/04. Stopwatch/stopwatch.js
+++ JS-advanced-september-2019/06. DOM Manipulations/Lab/04. Stopwatch/stopwatch.js
@@ -1,3 +1,18 @@
 function stopwatch() {
-    console.log('TODO:...');
+    const time = document.querySelector('#time');
+    let [ min, sec ] = time.innerHTML.split(':');
+    min = Number(min);
+    sec = Number(sec);
+    const stop = $('#stopBtn').click(() => {
+        $('#startBtn').removeAttr('disabled');
+        $('#stopBtn').attr('disabled', 'true');
+     
+
+    });
+    const start = $('#startBtn').click(() => {
+        $('#stopBtn').removeAttr('disabled');
+        $('#startBtn').attr('disabled', 'true');
+
+    });
+    console.log(min, sec); 
 }
